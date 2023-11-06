b872dae40f7cf5cd420a2226bcde45ff606489c3
FacuRM <fabrim15@gmail.com>
2023-11-06 16:18:04 -0600

    Prueba

---
+++ static/prueba.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+    socket.on('respuesta', function(respuesta) {
+        mostrarMensajeChatbot(respuesta);
+    });
+
+    function mostrarMensajeUsuario(mensaje) {
+        var chatContainer = document.getElementById("chat-container");
+        var mensajeUsuario = document.createElement("p");
+        mensajeUsuario.className = "mensaje-usuario";
+        mensajeUsuario.textContent = mensaje;
+        chatContainer.appendChild(mensajeUsuario);
+    }
+
+    function mostrarMensajeChatbot(mensaje) {
+        var chatContainer = document.getElementById("chat-container");
+        var mensajeChatbot = document.createElement("p");
+        mensajeChatbot.className = "mensaje-chatbot";
+        mensajeChatbot.textContent = mensaje;
+        chatContainer.appendChild(mensajeChatbot);
+    }
+
+    function enviarMensaje() {
+        var mensajeUsuario = document.getElementById("mensaje-usuario").value;
+        socket.emit('mensaje', mensajeUsuario);
+        mostrarMensajeUsuario(mensajeUsuario);
+    
+        // Vaciar el campo de texto después de 1 segundo
+        setTimeout(function() {
+            document.getElementById("mensaje-usuario").value = "";
+        }, 1000);
+    }
+    
+
+    // Agregar el evento click al botón
+    var enviarButton = document.getElementById("enviar-button");
+    enviarButton.addEventListener("click", enviarMensaje);
+
+    // Mensaje de bienvenida inicial
+    mostrarMensajeChatbot("Hola, soy Melody. ¿Dime que sintomas tienes?");
+});
